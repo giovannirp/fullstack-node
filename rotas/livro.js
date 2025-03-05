664fb4d432af56df71fab2193e5a39fabd30c5e1
giovanni <giovanni_ge@hotmai.com>
2025-03-04 21:04:16 -0300

    feat: adicionando organizações

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
+const { getLivros } = require("../controladores/livro");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Olá Gigi, novo!");
-});
+router.get("/", getLivros);
 
 router.post("/", (req, res) => {
   res.send("Você fez uma requisição do tipo POST");
