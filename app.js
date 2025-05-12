864a72d38dc1294545ea46813acee5f927d26f26
Giovanni <giovanni_ge@hotmail.com>
2025-05-12 13:05:53 -0300

    feat: adicionando new info

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const rotaLivros = require("./rotas/livro");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json()); // Permite que req.body funcione corretamente
+app.use(cors({origin: "*"}));
 
 
 const port = 8000;
